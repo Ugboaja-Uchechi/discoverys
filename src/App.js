0109b29640e5403b29d5aea153e237701b60bac0
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-05-11 17:08:24 +0100

    Add loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import Home from "./components/Home";
+import { ClimbingBoxLoader } from "react-spinners";
 
+const override = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100vh",
+  backgroundColor: "hsl(260, 8%, 14%)",
+};
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div>
-<Home />
+      {loading ? (
+        <ClimbingBoxLoader
+        cssOverride={override}
+          color={"#7cc4f0"}
+          loading={loading}
+          size={30}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <Home />
+      )}
+
     </div>
   );
 }
